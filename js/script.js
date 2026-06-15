f3a3d0d143d650b58401debfc8d711d39fd99cdc
Kayke Joaquim <113639746+kaykeky@users.noreply.github.com>
2026-06-15 10:50:52 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function validarLogin() {
+    let login = document.getElementById("login").value; 
+    let senha = document.getElementById("senha").value;
+
+    if (login === "admin" && senha === "123456") {
+    alert("Oi, " + login + " , meu padrin!");
+    window.location.href="produtos.html";
+ }  else {
+        alert("Acesso negado!");
+    }
+    
+     login = document.getElementById("login").value = '';
+     senha = document.getElementById("senha").value = '';
+}
